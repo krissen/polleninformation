6420b4b60f789190a3a196c8fd2c02a13b554bbc
Kristian Niemi <kristian.niemi@gmail.com>
2025-07-13 12:30:30 +0200

    (const_levels.py) Translations of levels for all supported languages

---
+++ custom_components/polleninformation/const_levels.py
@@ -0,0 +1,26 @@
+# const_levels.py
+
+LEVELS = {
+    0: ["keine Belastung", "gering", "mäßig", "hoch", "sehr hoch"],  # German
+    1: ["none", "low", "moderate", "high", "very high"],  # English
+    6: [  # Finnish
+        "ei esiintymää",
+        "vähäinen",
+        "kohtalainen",
+        "korkea",
+        "erittäin korkea",
+    ],
+    7: ["ingen", "låg", "måttlig", "hög", "mycket hög"],  # Swedish
+    8: ["aucune", "faible", "modérée", "élevée", "très élevée"],  # French
+    11: ["nessuna", "bassa", "moderata", "alta", "molto alta"],  # Italian
+    13: ["nav", "zems", "mērens", "augsts", "ļoti augsts"],  # Latvian
+    14: ["nėra", "maža", "vidutinė", "didelė", "labai didelė"],  # Lithuanian
+    19: ["brak", "niski", "umiarkowany", "wysoki", "bardzo wysoki"],  # Polish
+    20: ["nenhum", "baixo", "moderado", "alto", "muito alto"],  # Portuguese
+    22: ["нет", "низкий", "умеренный", "высокий", "очень высокий"],  # Russian
+    24: ["žiadny", "nízky", "mierny", "vysoký", "veľmi vysoký"],  # Slovak
+    26: ["ninguno", "bajo", "moderado", "alto", "muy alto"],  # Spanish
+    28: ["yok", "düşük", "orta", "yüksek", "çok yüksek"],  # Turkish
+    29: ["немає", "низький", "помірний", "високий", "дуже високий"],  # Ukrainian
+    30: ["nincs", "alacsony", "közepes", "magas", "nagyon magas"],  # Hungarian
+}
